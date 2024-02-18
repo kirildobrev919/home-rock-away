094c819f733b2e569b73bb38578912ad7af77d67
Kiril Dobrev <kirildobrev919@gmail.com>
2024-02-18 01:24:43 +0000

    Chap-1.3 Contact page added
    Unit tests for Contact page added

--- Rockaway.WebApp.Tests/Pages/PageTests.cs
+++ Rockaway.WebApp.Tests/Pages/PageTests.cs
@@ -12,6 +12,16 @@ namespace Rockaway.WebApp.Tests.Pages {
 			response.EnsureSuccessStatusCode();
 		}
 
+		[Theory]
+		[InlineData("/privacy")]
+		[InlineData("/contact")]
+		public async Task Index_Page_Returns_Success_WithTheory(string path) {
+			await using var factory = new WebApplicationFactory<Program>();
+			using var client = factory.CreateClient();
+			using var response = await client.GetAsync(path);
+			response.EnsureSuccessStatusCode();
+		}
+
 		[Fact]
 		public async Task HomePage_Title_Has_Correct_Content() {
 			var browsingContext = BrowsingContext.New(Configuration.Default);
@@ -26,6 +36,7 @@ namespace Rockaway.WebApp.Tests.Pages {
 
 		[Theory]
 		[InlineData("/", "Rockaway")]
+		[InlineData("/contact", "Contact Us")]
 		[InlineData("/privacy", "Privacy Policy")]
 		public async Task Page_Has_Correct_Title(string url, string title) {
 			var browsingContext = BrowsingContext.New(Configuration.Default);
@@ -36,6 +47,20 @@ namespace Rockaway.WebApp.Tests.Pages {
 			var titleElement = dom.QuerySelector("title");
 			titleElement.ShouldNotBeNull();
 			titleElement.InnerHtml.ShouldBe(title);
+		}
+
+		[Theory]
+		[InlineData("/contact")]
+		public async Task Page_Has_Correct_EmailAndPhoneNumber(string url) {
+			var browsingContext = BrowsingContext.New(Configuration.Default);
+			await using var factory = new WebApplicationFactory<Program>();
+			var client = factory.CreateClient();
+			var html = await client.GetStringAsync(url);
+			var dom = await browsingContext.OpenAsync(req => req.Content(html));
+			var listItems = dom.QuerySelectorAll("li");
+			listItems.ShouldNotBeNull();
+			listItems.ShouldContain(i => i.InnerHtml.Contains("hello@rockaway.dev"));
+			listItems.ShouldContain(i => i.InnerHtml.Contains("555 1234"));
 		}
 	}
 }
