9c81212903f280b3a2fed45a1a9df8f85c1d2182
Kiril Dobrev <kirildobrev919@gmail.com>
2024-02-23 16:50:54 +0000

    Chap-3.1 Formatting

--- Rockaway.WebApp/Pages/Contact.cshtml.cs
+++ Rockaway.WebApp/Pages/Contact.cshtml.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Rockaway.WebApp.Pages
-{
-    public class ContactModel : PageModel
+namespace Rockaway.WebApp.Pages {
+	public class ContactModel : PageModel
     {
         public void OnGet()
         {
