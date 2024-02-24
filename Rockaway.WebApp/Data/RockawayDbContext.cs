48512ce49dd3b9282f5e2baa101e2d578372c557
Kiril Dobrev <kirildobrev919@gmail.com>
2024-02-24 15:56:35 +0000

    Chap-3.3 Migrations added and staging table populated

--- Rockaway.WebApp/Data/RockawayDbContext.cs
+++ Rockaway.WebApp/Data/RockawayDbContext.cs
@@ -4,11 +4,22 @@ using Rockaway.WebApp.Data.Sample;
 
 namespace Rockaway.WebApp.Data;
 
-public class RockawayDbContext (DbContextOptions<RockawayDbContext> options) : DbContext(options) {
+public class RockawayDbContext(DbContextOptions<RockawayDbContext> options) : DbContext(options) {
 	public DbSet<Artist> Artists { get; set; } = default!;
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder) {
 		base.OnModelCreating(modelBuilder);
+
+		// Override EF Core's default table naming (which pluralizes entity names)
+		// and use the same names as the C# classes instead
+		foreach (var entity in modelBuilder.Model.GetEntityTypes()) {
+			entity.SetTableName(entity.DisplayName());
+		}
+
+		modelBuilder.Entity<Artist>(entity => {
+			entity.HasIndex(artist => artist.Slug).IsUnique();
+		});
+
 		modelBuilder.Entity<Artist>().HasData(SampleData.Artists.AllArtists);
 	}
 }
