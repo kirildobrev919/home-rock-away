c8c11f733d27bffb1dde97b66e4b74e8d09f9522
Kiril Dobrev <kirildobrev919@gmail.com>
2024-03-17 18:24:59 +0000

    Chap-4.2 Artists and Venues read only for user access

---
+++ Rockaway.WebApp/Controllers/VenuesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rockaway.WebApp.Data;
+
+namespace Rockaway.WebApp.Controllers {
+	public class VenuesController : Controller {
+		private readonly RockawayDbContext _context;
+
+		public VenuesController(RockawayDbContext context) {
+			_context = context;
+		}
+
+		// GET: Venues
+		public async Task<IActionResult> Index() {
+			return View(await _context.Venues.ToListAsync());
+		}
+
+		// GET: Venues/Details/5
+		public async Task<IActionResult> Details(Guid? id) {
+			if (id == null) {
+				return NotFound();
+			}
+
+			var venue = await _context.Venues
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (venue == null) {
+				return NotFound();
+			}
+
+			return View(venue);
+		}
+
+	}
